3ff051af5ed1d9c5e33d0b76910c8aec06f69747
m4drat <ftri2001@gmail.com>
2019-11-15 16:21:44 +0300

    Visualisation added. Unit tests updated.

--- HuffmanCoding/src/HuffmanCoding.py
+++ HuffmanCoding/src/HuffmanCoding.py
@@ -1,40 +1,97 @@
 from __future__ import annotations
 
 import collections
-import typing
 import heapq
+import copy
 
 from dataclasses import dataclass
+from graphviz import Digraph
 
 @dataclass
 class HuffmanCoding:
-    heap: list = None
+    vis_queue: list = None
+    sorted_queue: list = None
     hf_codes: dict = None
     default_freq_dict: dict = None
 
+    def visualize(self, sq_init=None) -> Digraph:
+        if sq_init == None:
+            sq_init = copy.deepcopy(self.vis_queue)
+        else:
+            sq_init = copy.deepcopy(sq_init)
+
+        digraph = Digraph()
+
+        if len(sq_init) == 1:
+            digraph.node(f"'{sq_init[0][1][0]}'", shape='circle')
+        else:
+            for node in sq_init:
+                digraph.node(name=node[1][0], label=f"'{node[1][0]}'", shape='circle')
+            # iterate over heap, while there is no more unused nodes
+            while len(sq_init) > 1:
+                min0 = heapq.heappop(sq_init) # extract char with smallest frequency
+                min1 = heapq.heappop(sq_init) # extract char with smallest frequency
+
+                # create new node
+                new_node = [min0[0] + min1[0]] + [[min0[1][0] + min1[1][0]]]
+
+                digraph.node(name=new_node[1][0], label='', shape='point')
+
+                digraph.edge(head_name=min0[1][0], tail_name=new_node[1][0], label='0')
+                digraph.edge(head_name=min1[1][0], tail_name=new_node[1][0], label='1')
+
+                # merge two nodes
+                heapq.heappush(sq_init, new_node)
+        
+        return digraph
+
     def normalize(self, msg: str) -> str:
+        '''
+        convert string to uppercase (in case we want to use dictionary with already
+            known char frequences, and there are only upper-case letters)
+        Parameters
+        ----------
+        msg: str
+            just a string with message
+        Returns
+        -------
+            upper-case string: str
+        '''
         return msg.upper()
 
-    def un_encode(self, msg: str):
+    def un_encode(self, msg: str) -> str:
         '''
-        params:
-            msg: str
-                huffman-coded message
-        return:
-            encoded string
+        to visually show, that message now is different
+        Parameters
+        ----------
+        msg: str
+            huffman-coded message
+        Returns
+        -------
+            encoded string: str
         '''
-        data = [msg[i:i+7] for i in range(0, len(msg), 7)]
-        return ''.join(chr(int(data[idx], 2)) for idx in range(0, len(data)))
+        data = [msg[i:i+8] for i in range(0, len(msg), 8)]
+
+        tmp = ''
+        for idx in range(0, len(data)):
+            # ascii_printable
+            if int(data[idx], 2) < 127 and int(data[idx], 2) > 31:
+                tmp += chr(int(data[idx], 2))
+            else:
+                tmp += f'\\x{hex(int(data[idx], 2))[2:].rjust(2, "0")}'
+        return tmp
 
     def decode(self, msg: str, freq: dict) -> str:
         '''
-        params:
+        Parameters
+        ----------
             msg: str
                 huffman-coded message
             freq: dict
-                dict with all char codes and their symbol 
-        return: str
-            decoded string
+                dict with all char codes and their symbol
+        Returns
+        -------
+            decoded string: str
         '''
 
         out = ''
@@ -51,10 +108,13 @@ class HuffmanCoding:
 
     def encode(self, msg: str) -> str:
         '''
+        Parameters
+        ----------
         msg: str
             input message
-        return: str
-            huffman encoded binary string
+        Returns
+        -------
+            huffman encoded binary string: str
         '''
         # convert string to uppercase
         # msg = self.normalize(msg)
@@ -70,28 +130,34 @@ class HuffmanCoding:
                 freq_dict[k] = self.default_freq_dict[k]
                 
         # convert freq_dict to heapq
-        self.heap = [[freq, [char, ""]] for char, freq in freq_dict.items()]
-        heapq.heapify(self.heap)
+        self.sorted_queue = [[freq, [char, ""]] for char, freq in freq_dict.items()]
+        heapq.heapify(self.sorted_queue)
 
-        # iterate over heap, while there is no more unused nodes
-        while len(self.heap) > 1:
-            min0 = heapq.heappop(self.heap) # extract char with smallest frequency
-            min1 = heapq.heappop(self.heap) # extract char with smallest frequency
+        self.vis_queue = copy.deepcopy(self.sorted_queue)
 
-            # update code value for char in heap
-            for pair in min0[1:]:
-                pair[1] = '0' + pair[1]
-            for pair in min1[1:]:
-                pair[1] = '1' + pair[1]
+        # there is only one letter in message
+        if len(self.sorted_queue) == 1:
+            self.sorted_queue[0][1][1] = '0'
+        else:
+            # iterate over heap, while there is no more unused nodes
+            while len(self.sorted_queue) > 1:
+                min0 = heapq.heappop(self.sorted_queue) # extract char with smallest frequency
+                min1 = heapq.heappop(self.sorted_queue) # extract char with smallest frequency
 
-            # merge two nodes
-            heapq.heappush(self.heap, [min0[0] + min1[0]] + min0[1:] + min1[1:])
+                # update code value for char in heap
+                for pair in min0[1:]:
+                    pair[1] = '0' + pair[1]
+                for pair in min1[1:]:
+                    pair[1] = '1' + pair[1]
+
+                # merge two nodes
+                heapq.heappush(self.sorted_queue, [min0[0] + min1[0]] + min0[1:] + min1[1:])
 
         # heap now is list
-        self.heap = heapq.heappop(self.heap)[1:]
+        self.sorted_queue = heapq.heappop(self.sorted_queue)[1:]
 
         # create dict from heap (key - char, value - huffman code)
-        self.hf_codes = dict(self.heap)
+        self.hf_codes = dict(self.sorted_queue)
 
         # convert to binary string
         return ''.join([self.hf_codes[char] for char in msg])
@@ -99,7 +165,11 @@ class HuffmanCoding:
 def main():
     hc = HuffmanCoding()
 
-    str_to_encode = 'Ларионов-Тришкин Теодор Арсений'
+    strs_to_encode = ['AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 
+                      'sacra ignis Inquisitionis devorabit vos', 
+                      'Лорем ипсум долор сит амет, харум медиоцритатем еа дуо, нобис хомеро аудиам не дуо.', 
+                      'FFFFFFFHHHDDDJJJJJJJJJQQQQQPEEEEEEELLLLLDDDS']
+    str_to_encode  = strs_to_encode[2]
 
     encoded = hc.encode(str_to_encode)
     decoded = hc.decode(encoded, hc.hf_codes)
@@ -107,8 +177,11 @@ def main():
     print(f'Encoded:   "{encoded}"')
     print(f'UnEncoded: "{hc.un_encode(encoded)}"')
     print(f'Decoded:   "{decoded}"')
+    print(f'HF_codes:  "{hc.hf_codes}"')
 
     print(f'str_to_encode == decoded: {str_to_encode == decoded}')
+
+    hc.visualize().view()
 
 if __name__ == '__main__':
     main()
