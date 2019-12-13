14ec42b26a5849618964f552212f49a96870e981
m4drat <ftri2001@gmail.com>
2019-12-13 13:27:13 +0300

    updated structure + gitignore

--- AdaptiveHuffmanCoding/src/exceptions.py
+++
@@ -1,7 +0,0 @@
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-class NotInitializedError(Error):
-    def __init__(self, msg):
-        Exception.__init__(self, msg)
